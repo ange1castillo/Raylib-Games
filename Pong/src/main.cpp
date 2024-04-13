db4da7354a8a893558fff2e9f6ccc3784d2c799b
ange1castillo <angelac1357@gmail.com>
2024-04-12 19:47:15 -0700

    Revert "Create Obstacle class"
    This reverts commit 04b111ac38a14e633f90c6673fcf42a90d2d3d33.

--- Pong/src/main.cpp
+++ Pong/src/main.cpp
@@ -2,221 +2,165 @@
 #include "raymath.h"
 
 #include <iostream>
-#include <vector>
 
-class Laser{
-    private:
-        Vector2 m_position {};
-        int m_speed {};
-        bool m_active {true};
+Color Green = Color {38, 185, 154, 255};
+Color Dark_Green = Color {20, 160, 133, 255};
+Color Light_Green = Color {129, 204, 184, 255};
+Color Yellow = Color {243, 213, 91, 255};
+
+class Player{
+    protected:
+        void stayInWindow(){
+            if(m_posY <= 0){
+                m_posY = 0;
+            }
+
+            if(m_posY + m_height >= GetScreenHeight()){
+                m_posY = GetScreenHeight() - m_height;
+            }
+        }
 
     public:
-        Laser(Vector2 position, int speed)
-            : m_position {position}, m_speed {speed}
-        {
+        float m_posX {};
+        float m_posY {};
+        float m_width {};
+        float m_height {};
+        int m_speedY {6};
+        Color m_color {};
+        int m_score {};
 
-        }
+        Player(float posX, float posY, float width, float height, Color color)
+            : m_posX {posX}, m_posY {posY}, m_width {width}, m_height {height}, m_color {color}{
 
-        bool isActive(){
-            return m_active;
-        }
-
-        void Update(){
-            m_position.y += m_speed;
-            if(m_active){
-                if(m_position.y < 0 || m_position.y > GetScreenHeight()){
-                    m_active = false;
-                    std::cout << "Laser Inactive" << '\n';
-                }
             }
-        }
-
-        void Draw(){
-            if(m_active){
-                DrawRectangle(m_position.x, m_position.y, 4, 15, YELLOW);
-            }
-        }
-};
-
-class Spaceship{
-    private:
-        Texture2D m_image {LoadTexture("Graphics/spaceship.png")};
-        Vector2 m_position {static_cast<float>(GetScreenWidth() - m_image.width) / 2, 
-                            static_cast<float>(GetScreenHeight() - m_image.height)};
-        int m_speed {7};
-        std::vector<Laser> m_lasers {};
-        double m_lastFiredTime {0.0};
     
+        void drawPaddle(){
+            DrawRectangleRounded({m_posX, m_posY, m_width, m_height}, 0.8, 0, WHITE);
+        }
+
+        void updatePaddle(){
+            if(IsKeyDown(KEY_UP)){
+                m_posY -= m_speedY;
+            }
+
+            if(IsKeyDown(KEY_DOWN)){
+                m_posY += m_speedY;
+            }
+
+            stayInWindow();
+        }
+};
+
+class CPU: public Player{
     public:
-        ~Spaceship(){
-            UnloadTexture(m_image);
-        }
+        CPU(float posX, float posY, float width, float height, Color color)
+            : Player {posX, posY, width, height, color}{
 
-        std::vector<Laser>& GetLasers() {
-            return m_lasers;
-        }
-
-        void Draw(){
-            DrawTextureV(m_image, m_position, WHITE);
-        }
-
-        void MoveLeft(){
-            m_position.x -= m_speed;
-
-            if(m_position.x <= 0){
-                m_position.x = 0;
             }
-        }
 
-        void MoveRight(){
-            m_position.x += m_speed;
-
-            if(m_position.x >= GetScreenWidth() - m_image.width){
-                m_position.x = static_cast<float>(GetScreenWidth() - m_image.width);
+        void updatePaddle(float ballPosY){
+            if(m_posY + m_height / 2 > ballPosY){
+                m_posY -= m_speedY;
             }
-        }
 
-        void shoot(){
-            if(GetTime() - m_lastFiredTime >= 0.35){
-                m_lasers.push_back(Laser({m_position.x + m_image.width / 2 - 2, m_position.y}, - 6));
-                m_lastFiredTime = GetTime();
+            if(m_posY + m_height / 2 <= ballPosY){
+                m_posY += m_speedY;
             }
+
+            stayInWindow();
         }
 };
 
-class Block{
-    private:
-        Vector2 m_position {};
-
+class Ball{
     public:
-        Block(Vector2 position)
-            : m_position {position}
-        {
+        float m_posX {};
+        float m_posY {};
+        int m_speedX {7};
+        int m_speedY {7};
+        int m_radius {};
+        Color m_color {};
+
+        Ball(float posX, float posY, int radius, Color color)
+            : m_posX {posX}, m_posY {posY}, m_radius {radius}, m_color {color}{
 
         }
-        void Draw(){
-            DrawRectangle(m_position.x, m_position.y, 3, 3, BLUE);
 
+        void draw(){
+            DrawCircle(m_posX, m_posY, m_radius, m_color);
+        }
+
+        void update(Player& player, CPU& cpu){
+            m_posX += m_speedX;
+            m_posY += m_speedY;
+
+            if(m_posY + m_radius >= GetScreenHeight() || m_posY - m_radius <= 0){
+                m_speedY *= -1;
+            }
+
+            if(m_posX + m_radius >= GetScreenWidth()){
+                player.m_score++;
+                reset();
+            }
+
+            if(m_posX - m_radius <= 0){
+                cpu.m_score++;
+                reset();
+            }
+        }
+
+        void reset(){
+            m_posX = GetScreenWidth() / 2;
+            m_posY = GetScreenHeight() / 2;
+
+            constexpr std::array<int, 2> speedChoices{-1, 1};
+            m_speedX *= speedChoices[GetRandomValue(0,1)];
+            m_speedY *= speedChoices[GetRandomValue(0,1)];
         }
 };
 
-class Obstacle{
-    private:
-        Vector2 m_position {};
-        std::vector<Block> m_blocks {};
-        std::vector<std::vector<int>> m_grid{};
+int main(){
+    constexpr int screen_width { 1280 };
+    constexpr int screen_height { 800 };
 
-    public: 
-        Obstacle(Vector2 position)
-            : m_position {position}
-        {
-            m_grid = {
-                {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
-                {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-                {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1}
-            };
-
-            for(std::size_t row{0}; row < m_grid.size(); ++row){
-                for(std::size_t column {0}; column < m_grid[row].size(); ++column){
-                    if(m_grid[row][column] == 1){
-                        float posX {m_position.x + column * 3};
-                        float posY {m_position.y + row * 3};
-                        m_blocks.push_back(Block({posX, posY}));
-                    }
-                }
-            }
-        }
-
-        void Draw(){
-            for(auto& block: m_blocks){
-                block.Draw();
-            }
-        }
-};
-
-class Game{
-    private:
-        Spaceship spaceship {};
-
-    public:
-        Game(){
-
-        }
-
-        ~Game(){
-
-        }
-
-        void Draw(){
-            spaceship.Draw();
-            for(auto& laser: spaceship.GetLasers()){
-                laser.Draw();
-            }
-        }
-
-        void Update(){
-            for(auto p{spaceship.GetLasers().begin()}; p != spaceship.GetLasers().end();){
-
-                p->Update();
-                if(!p->isActive()){
-                    spaceship.GetLasers().erase(p);
-                }
-                else{
-                    ++p;
-                }
-            }
-        }
-
-        void HandleInput(){
-            if(IsKeyDown(KEY_LEFT)){
-                spaceship.MoveLeft();
-            }
-
-            if(IsKeyDown(KEY_RIGHT)){
-                spaceship.MoveRight();
-            }
-
-            if(IsKeyDown(KEY_SPACE)){
-                spaceship.shoot();
-            }
-        }
-};
-
-int main(void) {
-    constexpr Color grey = {29, 29, 27, 255};
-    constexpr int windowWidth {750};
-    constexpr int windowHeight {700};
-
-    InitWindow(windowWidth, windowHeight, "Space Invaders");
+    InitWindow(screen_width, screen_height, "Pong");
     SetTargetFPS(60);
 
-    Game game {};
-    Obstacle obstacle {{100, 100}};
+    Ball ball {screen_width / 2, screen_height / 2, 20, Yellow};
+    Player player {10, (screen_height / 2) - 60, 25, 120, WHITE}; 
+    CPU cpu {screen_width - 35, (screen_height / 2) - 60, 25, 120, WHITE};
 
-    while(!WindowShouldClose()){
-
-        game.HandleInput();
-        game.Update();
-
+    while(!WindowShouldClose()) {
         BeginDrawing();
 
-        ClearBackground(grey);
+        ball.update(player, cpu);
+        cpu.updatePaddle(ball.m_posY);
+        player.updatePaddle();
 
-        game.Draw();
-        obstacle.Draw();
+        if(CheckCollisionCircleRec(Vector2{ball.m_posX, ball.m_posY}, ball.m_radius, 
+                                    Rectangle{player.m_posX, player.m_posY, player.m_width, player.m_height})){
+            ball.m_speedX *= -1;
+        }
 
+        if(CheckCollisionCircleRec(Vector2{ball.m_posX, ball.m_posY}, ball.m_radius, 
+                                    Rectangle{cpu.m_posX, cpu.m_posY, cpu.m_width, cpu.m_height})){
+            ball.m_speedX *= -1;
+        }
+
+        ClearBackground(Dark_Green);
+
+        DrawRectangle(screen_width / 2, 0, screen_width / 2, screen_height, Green);
+        DrawCircle(screen_width / 2, screen_height / 2, 150, Light_Green);
+        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+
+        ball.draw();
+        cpu.drawPaddle();
+        player.drawPaddle();
+        DrawText(TextFormat("%i", player.m_score), (screen_width / 4) - 20, 20, 80, WHITE);
+        DrawText(TextFormat("%i", cpu.m_score), (3 * screen_width / 4) - 20, 20, 80, WHITE);
+        
         EndDrawing();
     }
 
     CloseWindow();
+    return 0;
 }
